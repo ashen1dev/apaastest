94b359fd2bc02f5948d9f56c54328431696e9d46
ashen1dev <windswolf@gmail.com>
2020-09-22 10:22:05 +0900

    test

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,22 @@
 import os
 import requests
+import time
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
 
+
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES',3))
-    return HttpResponse('Hello! ' * times)
+    start = time.time()
+    sum = 0
+    for i in range(1, 100000000):
+        sum = sum + i
+    elapsed = time.time() - start
+    temp = HttpResponse('Hello! '+ str(elapsed))
+    return temp
 
 def db(request):
 
